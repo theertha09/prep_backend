4de2e958789b3270a9a74a897973ec48f53dbfb4
anuanaghavm <anaghavm2019@gmail.com>
2025-03-18 14:26:15 +0530

    update

--- prep/settings.py
+++ prep/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'blog',
     'contactus',
     'course',
-
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
