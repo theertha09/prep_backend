56022853a27476abbe07c08958259128c5062f89
anuanaghavm <anaghavm2019@gmail.com>
2025-03-19 13:01:38 +0530

    update

--- prep/settings.py
+++ prep/settings.py
@@ -57,6 +57,11 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5176",
+]
+
 ROOT_URLCONF = 'prep.urls'
 
 TEMPLATES = [
