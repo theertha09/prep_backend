3fb7ad5d28867931c8acd517a7e30a5f96a0a1b5
theertha09 <161118217+theertha09@users.noreply.github.com>
2025-03-19 12:18:05 +0530

    course

--- contactus/views.py
+++ contactus/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import status,generics
-from .models import Contact
-from .serializers import ContactSerializers
+from .models import Contact,MetaTagsContacts
+from .serializers import ContactSerializers,Contact_metadataSerializers
 
 class ContactListCreateAPIView(generics.ListCreateAPIView):
     queryset = Contact.objects.all()
@@ -11,3 +11,17 @@ class ContactRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView)
     queryset = Contact.objects.all()
     serializer_class = ContactSerializers
 
+class ContactMetaListView(generics.ListAPIView):
+    queryset = MetaTagsContacts.objects.all()
+    serializer_class = Contact_metadataSerializers
+
+class ContactMetaRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MetaTagsContacts.objects.all()
+    serializer_class = Contact_metadataSerializers
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # authentication_classes = [JWTAuthentication]
+
+    def get_object(self):
+        # Since we want only one contactmeta data record, always retrieve the first one
+        homemeta, created = MetaTagsContacts.objects.get_or_create(pk=1)
+        return homemeta
